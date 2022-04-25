0fe8c4f53b1153f4dd639bc54e6df545dfd7cfe1
Michael Pintos <machester4@gmail.com>
2022-04-25 09:08:17 -0300

    [FIX] limit query param parse

--- cmd/web/handle_get_top_products.go
+++ cmd/web/handle_get_top_products.go
@@ -6,10 +6,7 @@ import (
 )
 
 func handleGetTopProducts(ctx server.Context, s services.ProductLeadboard) error {
-	limit, err := ctx.GetQueryInt("limit")
-	if err != nil {
-		return err
-	}
+	limit, _ := ctx.GetQueryInt("limit")
 
 	products, err := s.GetTopProducts(ctx.Context(), limit)
 	if err != nil {
